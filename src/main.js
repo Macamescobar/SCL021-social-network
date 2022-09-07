c2fa4ecf9785d805dfa028637c48a63b3f64fbcf
paulasofiagc <paulasofiagc@gmail.com>
2022-09-07 12:05:36 -0300

    login y Account JS

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
-//import { myFunction } from './lib/index.js';
+import createLogin from "./login.js";
+import signUp from "./lib/account.js";
 
-import { login } from "./login.js";
-import { signUp } from "./lib/account.js";
-const divElement = document.getElementById("root");
-divElement.innerHTML = login;
+/* import { signUp } from "./lib/account.js"; */
+let root = () => {
+  let rootLogin = document.getElementById("root");
+  rootLogin.appendChild(signUp());
+};
 
-let account = document.getElementById("create-account");
-account.addEventListener("click", (e) => {
-  divElement.innerHTML = signUp;
-});
-
-let arrow = document.getElementById("arrow");
-arrow.addEventListener("click", (e) => {
-  divElement.innerHTML = login;
-});
+root();
